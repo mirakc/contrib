59de75ca9edc64879540a71b655593997fbe249b
removedporn <86824510+removedporn@users.noreply.github.com>
2023-04-24 20:57:53 +0800

    Delete filters directory

--- filters/README.md
+++
@@ -1,5 +0,0 @@
-# filters
-
-This folder contains filter scripts.
-
-The usage of each filter is described at the beginning of the script file.
--- filters/tests/test.sh
+++
@@ -1,9 +0,0 @@
-set -eu
-
-BASEDIR=$(cd $(dirname $0); pwd)
-
-ls -1 $BASEDIR/test-* | while read TEST
-do
-  echo "$TEST..."
-  sh $TEST
-done
